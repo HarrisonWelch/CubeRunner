0e2a5a1cde232c0fa9febba482a19d94f9d919bc
HarrisonWelch <hwelch1@my.apsu.edu>
2018-12-01 20:03:52 -0600

    init commit

---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class controller : MonoBehaviour {
+
+  public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+    moveSpeed = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    // print("updating...");
+    // float x = myObject.transform.position.x;
+    // float y = myObject.transform.position.y;
+    // float z = myObject.transform.position.z;
+    // print("Object pos = " + x + ", " + y + ", " + z);
+		
+    transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 
+                        0f,
+                        0f);
+
+    // print(Input.GetAxis("Horizontal"));
+
+    // if (Input.GetKey(KeyCode.W))
+    //   // print("W key was pressed");
+    //   // int = ;
+    //   // transform.Translate(0f, 1f*Time.deltaTime,0f);
+    // if (Input.GetKey(KeyCode.A))
+    //   // print("A key was pressed");
+    // if (Input.GetKey(KeyCode.S))
+    //   // print("S key was pressed");
+    // if (Input.GetKey(KeyCode.D))
+      // print("D key was pressed");
+    if (Input.GetKeyDown(KeyCode.Space)){
+      print("JUMP");
+      GetComponent<Rigidbody>().velocity =  new Vector3(0,8,0);
+    }
+	}
+
+  void OnCollisionEnter(Collision collision)
+  {
+    // foreach (ContactPoint contact in collision.contacts)
+    // {
+    //     Debug.DrawRay(contact.point, contact.normal, Color.white);
+    // }
+    if (collision.relativeVelocity.magnitude > 0.00001){
+        // audioSource.Play();
+      // print("COLLIDE! Time.deltaTime = " + Time.deltaTime);
+
+      // END GAME!
+      // enabled = false;
+      // DIE POTATO!!
+      Destroy(gameObject);
+      EditorUtility.DisplayDialog("YOU DIED","YOU DIED","Ok","Ok");
+    }
+    // if(collision.transform.gameObject.name == "DeathMaster")
+    // {
+    //   //do stuff
+      
+    //   print("COLLIDE! Time.deltaTime = " + Time.deltaTime);
+
+    //   // END GAME!
+    //   // enabled = false;
+    //   // DIE POTATO!!
+    //   Destroy(gameObject);
+    //   EditorUtility.DisplayDialog("YOU DIED","YOU DIED","A","B");
+    // }
+
+    // if( target.gameObject.name.Equals("DeathMaster") == true ){
+    //   print("COLLIDE");
+    // }
+  }
+}
---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+// using CenterSpace.NMath.Core;
+
+public class spawner : MonoBehaviour {
+  private float lastSpawnTimeDelta;
+  private List<GameObject> GameObjectList = new List<GameObject>();
+  public Transform[] spawnLocations;
+  public GameObject[] whatToSpawnPrefab;
+  public GameObject[] whatToSpawnClone;
+  System.Random rnd;
+  // Use this for initialization
+	void Start () {
+    rnd = new System.Random();
+    // spawnSomethingAwesomePlease();
+    lastSpawnTimeDelta = Time.fixedUnscaledTime;
+    // freezeRotation = true;
+	}
+  // void spawnSomethingAwesomePlease(){
+  //   for ( int i = 0; i < 5; i++ ){
+  //     whatToSpawnClone[i] = Instantiate(whatToSpawnPrefab[i], spawnLocations[i].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+  //     whatToSpawnClone[i].GetComponent<Rigidbody>().velocity = new Vector3(0f,
+  //                                                                          0f,
+  //                                                                          -1f);
+  //   }
+  //   // whatToSpawnClone[0].transform.position = new Vector3(0,0,0);
+  //   // whatToSpawnClone[0].GetComponent<Rigidbody>().velocity =  new Vector3(1,0,0);
+  // }
+
+  void randomSpawn(){
+    int i = rnd.Next(0, 5);
+    GameObject go = Instantiate(whatToSpawnPrefab[i], spawnLocations[i].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
+    whatToSpawnClone[i] = go;
+    GameObjectList.Add(go);
+    whatToSpawnClone[i].GetComponent<Rigidbody>().velocity = new Vector3(0f,
+                                                                         0f,
+                                                                         -3f);
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    if (lastSpawnTimeDelta + 1 < Time.fixedUnscaledTime){
+      randomSpawn();
+      lastSpawnTimeDelta = Time.fixedUnscaledTime;
+      // print("lastSpawnTimeDelta = " + lastSpawnTimeDelta);
+      print("whatToSpawnClone.Length = " + whatToSpawnClone.Length);
+      // foreach ( GameObject go in GameObjectList ) {
+      //   if ( GameObjectList[go].transform.position.y < -10f ) {
+      //     print("delete object");
+      //     GameObject delGo = GameObjectList[go];
+      //     GameObjectList.Remove(GameObjectList[go]);
+      //   }
+      // }
+    }
+	}
+}
